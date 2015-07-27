07fb02a50563db276c8f9d5a6b6c7af4553923fc
Pavel Boliybash <pavel.b@live.ru>
2015-07-27 13:41:16 +0300

    AB

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -70,24 +70,24 @@ namespace ConsoleApplication1
             //// TEST ECRfor1C
 
             int mils = DateTime.Now.Millisecond;
-            ECR ecr1c = new ECR();
+            ECRfor1C ecr1c = new ECRfor1C();
 
 
 
             Console.WriteLine(ecr1c.Connect("192.168.88.53", 5999, 1));
 
-            List<string> list = ecr1c.SearchReceipt("25-07-15 00:00:00 DST", "25-07-15 23:00:00 DST");
+            //List<string> list = ecr1c.SearchReceipt("25-07-15 00:00:00 DST", "25-07-15 23:00:00 DST");
 
-            Console.WriteLine(list[0]);
-            Console.WriteLine(list[1]);
-            Console.WriteLine(list[2]);
-            Console.WriteLine(list[3]);
+            //Console.WriteLine(list[0]);
+            //Console.WriteLine(list[1]);
+            //Console.WriteLine(list[2]);
+            //Console.WriteLine(list[3]);
 
 
 
-            mils = mils - DateTime.Now.Millisecond;
+            //mils = mils - DateTime.Now.Millisecond;
 
-            Console.WriteLine(mils);
+            //Console.WriteLine(mils);
 
             //ecr1c.DataSales = "22-05-15";
             //while (ecr1c.GetCheck())
@@ -110,27 +110,27 @@ namespace ConsoleApplication1
             //}
 
 
-            //ecr1c.DataSalesFrom = "25-07-15";
-            //ecr1c.DataSalesTo = "25-07-15";
-            //while (ecr1c.GetCheck())
-            //{
-            //    Console.WriteLine("\n Чек - " + ecr1c.JCheckNum + "  Дата - " + ecr1c.JCheckDate + "\n  Скидка на чек " + ecr1c.JCheckDis + "%" + " Возврат - " + ecr1c.JCheckIsReturn + " Отменен -" + ecr1c.JCheckIsVoid + " Z -" + ecr1c.JCheckNumZRep);
-            //    while (ecr1c.ReadSales())
-            //    {
-            //        Console.WriteLine("\t" + ecr1c.JArtCode + " " + ecr1c.JArtName + " " + ecr1c.JArtPrice + " " + ecr1c.JArtQnt + " " + ecr1c.JArtSum + " " + ecr1c.JArtDis + " " + ecr1c.JArtVoid);
-            //    }
-            //    Console.WriteLine("Количество Оплат = " + ecr1c.JCheckPayCount);
-            //    Console.WriteLine("ВИД Оплаты1 - " + ecr1c.JCheckPay1Type);
-            //    Console.WriteLine("Сумма Оплаты1 - " + ecr1c.JCheckPay1Sum);
-            //    Console.WriteLine("ВИД Оплаты2 - " + ecr1c.JCheckPay2Type);
-            //    Console.WriteLine("Сумма Оплаты2 - " + ecr1c.JCheckPay2Sum);
-            //    Console.WriteLine("ВИД Оплаты3 - " + ecr1c.JCheckPay3Type);
-            //    Console.WriteLine("Сумма Оплаты3 - " + ecr1c.JCheckPay3Sum);
-            //    Console.WriteLine("Сумма Чека - " + ecr1c.JCheckSum);
-            //    Console.WriteLine("Tax1 - " + ecr1c.JCheckTax1Sum);
-            //    Console.WriteLine("Tax1Zbir - " + ecr1c.JCheckTax1Zbir);
+            ecr1c.DataSalesFrom = "25-07-15";
+            ecr1c.DataSalesTo = "25-07-15";
+            while (ecr1c.GetCheck())
+            {
+                Console.WriteLine("\n Чек - " + ecr1c.JCheckNum + "  Дата - " + ecr1c.JCheckDate + "\n  Скидка на чек " + ecr1c.JCheckDis + "%" + " Возврат - " + ecr1c.JCheckIsReturn + " Отменен -" + ecr1c.JCheckIsVoid + " Z -" + ecr1c.JCheckNumZRep);
+                while (ecr1c.ReadSales())
+                {
+                    Console.WriteLine("\t" + ecr1c.JArtCode + " " + ecr1c.JArtName + " " + ecr1c.JArtPrice + " " + ecr1c.JArtQnt + " " + ecr1c.JArtSum + " " + ecr1c.JArtDis + " " + ecr1c.JArtVoid);
+                }
+                Console.WriteLine("Количество Оплат = " + ecr1c.JCheckPayCount);
+                Console.WriteLine("ВИД Оплаты1 - " + ecr1c.JCheckPay1Type);
+                Console.WriteLine("Сумма Оплаты1 - " + ecr1c.JCheckPay1Sum);
+                Console.WriteLine("ВИД Оплаты2 - " + ecr1c.JCheckPay2Type);
+                Console.WriteLine("Сумма Оплаты2 - " + ecr1c.JCheckPay2Sum);
+                Console.WriteLine("ВИД Оплаты3 - " + ecr1c.JCheckPay3Type);
+                Console.WriteLine("Сумма Оплаты3 - " + ecr1c.JCheckPay3Sum);
+                Console.WriteLine("Сумма Чека - " + ecr1c.JCheckSum);
+                Console.WriteLine("Tax1 - " + ecr1c.JCheckTax1Sum);
+                Console.WriteLine("Tax1Zbir - " + ecr1c.JCheckTax1Zbir);
 
-            //}
+            }
       
 
 
